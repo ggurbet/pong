48847d9cc06b0574c13de65bb23880b0b77cd802
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2018-12-20 00:16:34 +0300

    Add border checking

--- pong.py
+++ pong.py
@@ -8,7 +8,7 @@ import turtle
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("#282c34")
-wn.setup(width = 800, height = 800)
+wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
 # Player 1
@@ -36,6 +36,9 @@ ball.shape("square")
 ball.color("#e9e9e9")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2 # each move is 2 pixels horizontally
+ball.dy = 2 # each move is 2 pixels vertically
+# combined effect is diagonal 2x2 movement
 
 # Player Movements
 def player1_up():
@@ -58,6 +61,9 @@ def player2_down():
     y -= 20
     player2.sety(y)
 
+# Ball movement
+
+
 # Keyboard controls
 wn.listen()
 wn.onkeypress(player1_up, "w")
@@ -68,3 +74,21 @@ wn.onkeypress(player2_down, "Down")
 # Main loop
 while True:
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Check borders
+    if ball.ycor() > 290:
+        ball.sety(290) # do not go out of bounds
+        ball.dy *= -1 # reverse direction
+    if ball.ycor() < -290:
+        ball.sety(-290) # do not go out of bounds
+        ball.dy *= -1 # reverse direction
+    if ball.xcor() > 390:
+        ball.goto(0, 0) # ball is out
+        ball.dx *= -1 # reverse direction
+    if ball.xcor() < -390:
+        ball.goto(0, 0) # ball is out
+        ball.dx *= -1 # reverse direction
